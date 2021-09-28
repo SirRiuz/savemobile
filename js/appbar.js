9c908b09c9e962783ed3a3a10473c7fd7b384d6c
Mateo <sirvegas21@gmail.com>
2021-09-28 07:42:30 -0500

    Mejorando

--- js/appbar.js
+++ js/appbar.js
@@ -1,4 +1,12 @@
 
 
 
+if(window.location.search.split('=')[1]>1) {
+  document.getElementById('header-figure').style.display = 'none'
+  document.getElementById('header-container').style.display = 'none'
+}
+
+
+
+
 
